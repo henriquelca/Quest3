54aa7501461c3263479b432afdbdd6c80dd3d15a
Henrique <henriquelca@hotmail.com>
2026-05-19 19:35:34 -0300

    fix: direção/ add: imagems

--- Cenas/animated_sprite_2d.gd
+++ Cenas/animated_sprite_2d.gd
@@ -2,22 +2,31 @@ extends AnimatedSprite2D
 
 class_name PlayerAnimatedSprite
 
-func trigger_animation(velocity: Vector2, direction: int, player_mode: Player.PlayerMode):
+@onready var original_scale_x: float = abs(scale.x)
+
+func trigger_animation(
+	velocity: Vector2,
+	direction: int,
+	player_mode: Player.PlayerMode
+):
 	var animation_prefix = Player.PlayerMode.keys()[player_mode].to_snake_case()
-	
+
+	# Flip sprite while preserving original scale
+	if direction != 0:
+		scale.x = original_scale_x * direction
+
+	# Jump
 	if not get_parent().is_on_floor():
 		play("%s_jump" % animation_prefix)
-		
-	elif sign(velocity.x) != sign(direction) && velocity.x != 0 && direction != 0:
+
+	# Slide
+	elif sign(velocity.x) != sign(direction) \
+	and velocity.x != 0 \
+	and direction != 0:
 		play("%s_slide" % animation_prefix)
-		scale.x = direction
-		
+
+	# Run / Idle
 	else:
-		if scale.x == 1 && sign(velocity.x) == -1:
-			scale.x = -1
-		elif scale.x == -1 && sign(velocity.x) == 1:
-			scale.x = 1
-			
 		if velocity.x != 0:
 			play("%s_run" % animation_prefix)
 		else:
